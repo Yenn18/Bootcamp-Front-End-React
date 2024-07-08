45a0c2283ffee9c0d7dd475532783a31ce74abe9
jenny <jenniferodriguezgonzalez@gmail.com>
2024-07-08 10:49:56 +0200

    8 julio

--- React/EjercicioReact/src/components/Footer.tsx
+++ React/EjercicioReact/src/components/Footer.tsx
@@ -5,9 +5,9 @@ export default function FooterApp (){
     return(
         <>
         <nav>
-            <a href="https://es-es.facebook.com/"><BsFacebook /></a>
-            <a href="https://www.instagram.com/"><AiOutlineInstagram /></a>
-            <a href="https://www.linkedin.com/"><AiFillLinkedin /></a>
+            <a href="https://es-es.facebook.com/"  target= "_blank"  rel= ""><BsFacebook /></a>
+            <a href="https://www.instagram.com/" target= "_blank"  rel="nooder"><AiOutlineInstagram /></a>
+            <a href="https://www.linkedin.com/" target= "_blank" rel="nooder" ><AiFillLinkedin /></a>
         </nav>   
         
         </>
